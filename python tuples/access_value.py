d9a3144ef76b8cee4a4e26a4c1afbe2307431c3b
Muasya0254 <ephraim.muasya@kibo.school>
2023-05-24 16:20:45 +0300

    Access values from the tuple

---
+++ python tuples/access_value.py
@@ -0,0 +1,15 @@
+#  Access values from the tuple
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+
+# values "orange" from the tuple
+orange = tuple1[0]
+
+# values 30 from the tuple
+thirty = tuple1[1][2]
+
+# values 15 from the tuple
+fifteen = tuple1[2][1]
+
+print(orange)
+print(thirty)
+print(fifteen)
